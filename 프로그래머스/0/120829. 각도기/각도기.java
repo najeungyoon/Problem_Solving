c151b941c98cb056bb1ba12f26b1f21ec92ce5d7
najeungyoon <162659262+najeungyoon@users.noreply.github.com>
2025-09-04 18:12:08 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -1,12 +1,31 @@
 class Solution {
     public int solution(int angle) {
+        int ans=0;
+        String gak = null;
+        
         if(0<angle && angle<90)
-            return 1;
+            gak="yegak";
         else if(angle==90)
-            return 2;
+            gak="jikgak";
         else if(90<angle && angle<180)
-            return 3;
+            gak="dungak";
         else
-            return 4;
+            gak="peonggak";
+        
+        switch(gak){
+            case "yegak":
+                ans=1;
+                break;
+            case "jikgak":
+                ans=2;
+                break;
+            case "dungak":
+                ans= 3;
+                break;
+            case "peonggak":
+                ans= 4;
+                break;
+        }
+        return ans;
     }
 }
